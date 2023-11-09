6379a9531dec6ec1245db96eb4bc50b832a3f0c2
Evgeny Goncharov <evgeny.goncharov.pro@gmail.com>
2023-11-09 18:29:53 +0100

    Destructured Api object (returned it without type and difficulty), parsed the result through local storage, added JS to index.html and results.html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
+const gameContainer = document.getElementById("gameContainer");
 
+const startGame = async () => {
+  const response = await axios.get(
+    "https://opentdb.com/api.php?amount=10&difficulty=hard&type=multiple"
+  );
+  const results = response.data.results;
+  console.log(results);
 
-async const startGame() {
+  const filteredResults = results.map((resultObject) => {
+    const filteredResult = {
+      category: resultObject.category,
+      question: resultObject.question,
+      correct_answer: resultObject.correct_answer,
+      incorrect_answers: resultObject.incorrect_answers,
+    };
+    return filteredResult;
+  });
+  localStorage.setItem("quizData", JSON.stringify(filteredResults));
+  document.location.href = "questions.html";
+};
 
-
-    document.open("/questions")
-}
+const quizData = JSON.parse(localStorage.getItem("quizData"));
+console.log(quizData);
