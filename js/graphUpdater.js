f9c6f971e9b7feab3d8cbfcf03a14f8cae184378
Evgeny Goncharov <evgeny.goncharov.pro@gmail.com>
2023-11-16 11:11:28 +0100

    Cambiado color de borde del grafico

--- js/graphUpdater.js
+++ js/graphUpdater.js
@@ -1,38 +1,39 @@
 // Importar la librería Chart.js
 
-
 // Función para crear y actualizar el gráfico
 const crearGrafico = (fechas, aciertos) => {
-    // Obtén una referencia al elemento canvas
-    const chartCanvas = document.getElementById('myChart');
+  // Obtén una referencia al elemento canvas
+  const chartCanvas = document.getElementById("myChart");
 
-    // Crea un contexto 2D para el gráfico
-    const ctx = chartCanvas.getContext('2d');
-    console.log("HOLA")
-    // Crea el gráfico utilizando Chart.js
-    new Chart(ctx, {
-        type: 'line', // Puedes elegir el tipo de gráfico que mejor se adapte a tus necesidades
-        data: {
-            labels: fechas,
-            datasets: [{
-                label: 'Cantidad de aciertos',
-                data: aciertos,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)', // Color de fondo del gráfico
-                borderColor: 'rgba(75, 192, 192, 1)', // Color del borde del gráfico
-                borderWidth: 1 // Ancho del borde del gráfico
-            }]
+  // Crea un contexto 2D para el gráfico
+  const ctx = chartCanvas.getContext("2d");
+  console.log("HOLA");
+  // Crea el gráfico utilizando Chart.js
+  new Chart(ctx, {
+    type: "line", // Puedes elegir el tipo de gráfico que mejor se adapte a tus necesidades
+    data: {
+      labels: fechas,
+      datasets: [
+        {
+          label: "Cantidad de aciertos",
+          data: aciertos,
+          backgroundColor: "rgba(75, 192, 192, 0.2)", // Color de fondo del gráfico
+          borderColor: "rgba(255, 255, 0, 1)", // Color del borde del gráfico
+          borderWidth: 1, // Ancho del borde del gráfico
         },
-        options: {
-            scales: {
-                y: {
-                    type: 'linear',
-                    ticks: {
-                        precision: 0 // Set precision to 0 to display only integers
-                    }
-                }
-            }
-        }
-    });
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          type: "linear",
+          ticks: {
+            precision: 0, // Set precision to 0 to display only integers
+          },
+        },
+      },
+    },
+  });
 };
 
 // Exportar la función del gráfico para poder utilizarla en otros archivos
